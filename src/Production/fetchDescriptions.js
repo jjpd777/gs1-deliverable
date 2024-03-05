7b6bf69c06cb9550a68f044b881eef5682592049
jjpd777 <juanjo.palacioduque@gmail.com>
2024-03-05 06:35:44 -0600

    Update fetchDescriptions.js
    Let's go

--- src/Production/fetchDescriptions.js
+++ src/Production/fetchDescriptions.js
@@ -1,5 +1,5 @@
 import { callBackendCompletion } from "./config/utils";
-
+// code
 export const bulletpointsPrompt = '\nCrear un conjunto de 3 a 5 "bulletpoints" para describir los atributos de este producto. Utiliza el character "•" para denotar un bulletpoint\n';
 export const keywordsPrompt = '\nCrea una lista de 5 palabras claves correspondientes a este producto. Proveer el resultado como usando "&&&" como bulletpoints . Formato esperado del resultado: [ palabra clave 1 &&& palabra clave 2 &&& palabra clave 3\n\n'
 
